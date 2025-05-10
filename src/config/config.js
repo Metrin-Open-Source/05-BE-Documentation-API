96cf1b8eacb7261c09728eab5261eb15b7eb276b
Habiboys <nouvalhabibie18@gmail.com>
2025-05-10 20:27:52 +0700

    resolve error

--- src/config/config.js
+++ src/config/config.js
@@ -17,5 +17,19 @@ module.exports = {
       },
     }
   },
+  test: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: 'database_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: 'database_production',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'mysql'
+  }
 }
 
